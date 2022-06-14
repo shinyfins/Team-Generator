0ebbd1c60b51a31cfa1c66a1999b816ba3cdef69
Your Name <mcpgco@gmail.com>
2022-06-14 12:46:09 -0700

    finished prompts for all types

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 
-const Employee = require("./lib/Employee");
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -9,13 +9,13 @@ inquirer.prompt([
     {
       name: "type",
       type: "list",
-      message: "Which employee first?",
-      choices: ["Employee", "Intern", "Manager", "Engineer"],
+      message: "Type of employee?",
+      choices: ["Intern", "Manager", "Engineer", "No more"],
     },
   ])
   .then((answer) => {
       switch (answer.type) {
-    case "Employee":
+    case "Manager":
         inquirer
         .prompt([
             {
@@ -34,15 +34,78 @@ inquirer.prompt([
             name: "Email",
             type: "input",
             message:  "What is the Employee's Email?"
+            },
+            {
+                name: "Office_Number",
+                type: "number",
+                message: "What is the Manager office number?"
             }
             
         ])
         .then((answer) => {
-            employee = new Employee(answer.name, answer.ID, answer.Email)
+            manager = new Manager(answer.name, answer.ID, answer.Email, answer.Office_Number);
         });
+
     case "Intern":
-    case "Manager":
+        inquirer
+        .prompt([
+            {
+            name: "name",
+            type: "input",
+            message: "What is your name?",
+            },
+
+            {
+            name: "ID",
+            type: "number",
+            message:  "What is the Employee's ID?"
+            },
+
+            {
+            name: "Email",
+            type: "input",
+            message:  "What is the Employee's Email?"
+            },
+            {
+            name: "School",
+            type: "input",
+            message: "What is the Interns School?"
+            }
+            
+        ])
+        .then((answer) => {
+            intern = new Intern(answer.name, answer.ID, answer.Email, answer.School);
+        });
     case "Engineer":
+        inquirer
+        .prompt([
+            {
+            name: "name",
+            type: "input",
+            message: "What is your name?",
+            },
+
+            {
+            name: "ID",
+            type: "number",
+            message:  "What is the Employee's ID?"
+            },
+
+            {
+            name: "Email",
+            type: "input",
+            message:  "What is the Employee's Email?"
+            },
+            {
+                name: "Github",
+                type: "input",
+                message: "What is their Github?"
+            }
+            
+        ])
+        .then((answer) => {
+            engineer = new Engineer(answer.name, answer.ID, answer.Email, answer.Github);
+        });
     default: 
       
 }
